23e24e6deb3ac187afb532e5663354657dd2383c
vhalenn <hennetier.valentin@gmail.com>
2021-10-03 00:15:21 +0200

    Adding Frame swap

--- Assets/Scripts/Gameplay/PlayerPhotoMode.cs
+++ Assets/Scripts/Gameplay/PlayerPhotoMode.cs
@@ -1,11 +1,59 @@
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 
 public class PlayerPhotoMode : MonoBehaviour
 {
     public CinemachineVirtualCamera photoModeCamera;
     public CanvasGroup photoModeCanvas;
-    public Vector2 focal;
+
+    [Header("Ranges")]
+    public Vector2 fov;
+    public float camAngle;
+    public float exposure;
+    public Canvas frames;
+
+    // VOLUME
+    [Header("PostProcess")]
+    [SerializeField] private Volume photoModeVolume;
+    private VolumeProfile photoModeVolumeProfile;
+    private ColorAdjustments colorAdj;
+    private DepthOfField dof;
+
+    //Storage
+    private Tween canvasAnim;
+    private GameObject[] frameArray;
+    private int actualFrame = 0;
+
+    private void Awake()
+    {
+        InitPostProcess();
+        InitFrames();
+
+        photoModeCanvas.alpha = 0;
+    }
+
+    void InitFrames()
+    {
+        Transform fTransform = frames.transform;
+        frameArray = new GameObject[fTransform.childCount];
+        actualFrame = 0;
+
+        for (int i = 0; i < fTransform.childCount; i++)
+        {
+            frameArray[i] = fTransform.GetChild(i).gameObject;
+            frameArray[i].SetActive(i == actualFrame);
+        }
+    }
+
+    void InitPostProcess()
+    {
+        photoModeVolumeProfile = photoModeVolume.profile;
+        photoModeVolumeProfile.TryGet<DepthOfField>(out dof);
+        photoModeVolumeProfile.TryGet<ColorAdjustments>(out colorAdj);
+    }
 
     void Update()
     {
@@ -17,18 +65,54 @@ public class PlayerPhotoMode : MonoBehaviour
 
     void FreezeTime()
     {
-        if (Time.timeScale > 0.5f) Time.timeScale = 0;
-        else Time.timeScale = 1;
+        canvasAnim.Complete();
+
+        if (Time.timeScale > 0.5f) // SHOW PHOTO MODE
+        {
+            canvasAnim = photoModeCanvas.DOFade(1, 0.2f).SetUpdate(true);
+            Time.timeScale = 0;
+        }
+        else // HIDE PHOTO MODE
+        {
+            Time.timeScale = 1;
+            canvasAnim = photoModeCanvas.DOFade(0, 0.2f);
+            ResetParams();
+        }
+    }
+
+    private void ResetParams()
+    {
+        ViewRoll(0.5f);
     }
 
     public void ChangeFocal(float value)
     {
-        float newFOV = Mathf.Lerp(focal.x, focal.y, value);
+        float newFOV = Mathf.Lerp(fov.x, fov.y, value);
         photoModeCamera.m_Lens.FieldOfView = newFOV;
     }
 
-    public void ViewRoll(float slider)
+    public void ViewRoll(float value)
     {
-        //photoModeCamera.m_Lens.Dutch = (slider.value - slider.minValue) / (slider.maxValue - slider.minValue) * (viewRoll.max - viewRoll.min) + viewRoll.min;
+        photoModeCamera.m_Lens.Dutch = Mathf.Lerp(-camAngle, camAngle, value);
+    }
+    public void Exposure(float value)
+    {
+        colorAdj.postExposure.Override(Mathf.Lerp(-exposure, exposure, value));
+    }
+
+    public void Aperture(float value)
+    {
+        //dof.aperture.Override((slider.value - slider.minValue) / (slider.maxValue - slider.minValue) * (aperture.max - aperture.min) + aperture.min);
+    }
+
+    public void NextFrame()
+    {
+        actualFrame++;
+        if (actualFrame >= frameArray.Length) actualFrame = 0;
+
+        for (int i = 0; i < frameArray.Length; i++)
+        {
+            frameArray[i].SetActive(i == actualFrame);
+        }
     }
 }
